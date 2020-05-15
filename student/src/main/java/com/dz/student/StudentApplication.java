17a56fad1454a4c488caaf69655ea34f1699d734
Soni Chahal <soni.chahal112@gmail.com>
2020-05-15 17:30:44 +0530

    Docker-compose for all services

--- student/src/main/java/com/dz/student/StudentApplication.java
+++ student/src/main/java/com/dz/student/StudentApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
